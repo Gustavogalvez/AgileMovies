a0888e72f41f519ee51cdba3323643827a8831f0
Gustavogalvez <44412673+Gustavogalvez@users.noreply.github.com>
2021-02-20 08:35:46 +0000

    Deploying to master from  @ 6f94fe30ce7a4f189c16b261faf6f5160ca47459 🚀

--- src/app/shared/sidenav/side-content/sidenav.model.ts
+++
@@ -1,9 +0,0 @@
-export class SideNav {
-  name!: string;
-  idx?: string;
-  icon?: string;
-  show?: boolean;
-  route?: string;
-  childs?: Array<SideNav>;
-  canRemove?: boolean;
-}
--- src/environments/environment.prod.ts
+++
@@ -1,9 +0,0 @@
-export const environment = {
-  production: true,
-
-  API_URL: {
-    protocol: 'http://',
-    domain: '161.35.140.236:9005',
-    suffix: '/api'
-  }
-};
